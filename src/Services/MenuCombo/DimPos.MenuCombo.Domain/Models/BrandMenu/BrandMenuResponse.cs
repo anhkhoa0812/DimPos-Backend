5196fbaa5e033925ae29c31a8e6aa9deb68b0d85
anhkhoa0812 <tahoanganhkhoa2014@gmail.com>
2025-05-14 18:55:57 +0700

    feat: Add Assign Store Menu API

---
+++ src/Services/MenuCombo/DimPos.MenuCombo.Domain/Models/BrandMenu/BrandMenuResponse.cs
@@ -0,0 +1,14 @@
+using DimPos.MenuCombo.Domain.Enums;
+
+namespace DimPos.MenuCombo.Domain.Models.BrandMenu;
+
+public record BrandMenuResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public EBrandMenuType? Type { get; set; }
+    public bool? IsActiveByBrand { get; set; }
+    public DateTime? ValidFrom { get; set; }
+    public DateTime? ValidTo { get; set; }
+}
