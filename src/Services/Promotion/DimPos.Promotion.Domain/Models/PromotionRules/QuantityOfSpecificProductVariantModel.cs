9e9dc27736390c49d4c3465fdc4e30b94fbb3eb6
anhkhoa0812 <tahoanganhkhoa2014@gmail.com>
2025-07-12 00:04:22 +0700

    fix: add JSON property names for ProductVariantId and Quantity in QuantityOfSpecificProductVariantModel

--- src/Services/Promotion/DimPos.Promotion.Domain/Models/PromotionRules/QuantityOfSpecificProductVariantModel.cs
+++ src/Services/Promotion/DimPos.Promotion.Domain/Models/PromotionRules/QuantityOfSpecificProductVariantModel.cs
@@ -1,7 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace DimPos.Promotion.Domain.Models.PromotionRules;
 
 public class QuantityOfSpecificProductVariantModel
 {
+    [JsonPropertyName("productVariantId")]
     public Guid ProductVariantId { get; set; }
+    [JsonPropertyName("quantity")]
     public int Quantity { get; set; }
 }
