b57abc50a8ab230d060abc2c7b80c44aadc3a226
anhkhoa0812 <tahoanganhkhoa2014@gmail.com>
2025-07-13 12:50:15 +0700

    feat: Add api to get all financial shifts and get financial shift by id

---
+++ src/Services/Store/DimPos.Store.Domain/Models/Response/GetFinancialShiftsResponse.cs
@@ -0,0 +1,23 @@
+using DimPos.Store.Domain.Enums;
+
+namespace DimPos.Store.Domain.Models.Response;
+
+public class GetFinancialShiftsResponse
+{
+    public Guid Id { get; set; }
+    public DateTime OpeningTimestamp { get; set; }
+    public Guid OpenedByAccountId { get; set; } 
+    public decimal OpeningCashExpected { get; set; }
+    public decimal OpeningCashActual { get; set; }
+    public string? OpeningDifferenceReason { get; set; }
+    public DateTime? ClosingTimestamp { get; set; }
+    public Guid? ClosedByAccountId { get; set; }
+    public decimal? TotalGrossSalesInShift { get; set; }
+    public decimal? TotalNetSalesInShift { get; set; }
+    public decimal? TotalTaxInShift { get; set; }
+    public decimal? TotalDiscountInShift { get; set; }
+    public decimal? TotalCashRoundingInShift { get; set; }
+    public EFinancialShiftStatus Status { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
+}
